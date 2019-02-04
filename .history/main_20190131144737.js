360cf10d5947fa88fabc558e621e28e4778d0a2c
marticam <pablo_cmm@hotmail.com>
2019-02-04 11:10:52 -0600

    main.js updated with the new logic

--- .history/main_20190131144737.js
+++ .history/main_20190131144737.js
@@ -1,7 +1,14 @@
+// // load the http module
+// var http = require('http');
+
+// // configure our HTTP server
+// var server = http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.end("Hello People ! Como van ! todo bien \n");
+// });
 
 var http = require('http');
 var fs = require('fs');
-
 var server = http.createServer(function (req, res) {
   fs.readFile('index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
